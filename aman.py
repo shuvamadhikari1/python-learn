c69ee70edfa70496628462667c7a81018597f275
HP <shuvamadhikari74@gmail.com>
2025-08-31 00:41:11 +0545

    all list

--- aman.py
+++ aman.py
@@ -1,41 +1,57 @@
-# i=[1,2,3,4,5,6,7,8,9,10]
-# for a in i:
-#     if a%2==0:  
-#         print(f"{a} is even")
-#     else:   
-#         print(f"{a} is odd") 
+# # i=[1,2,3,4,5,6,7,8,9,10]
+# # for a in i:
+# #     if a%2==0:  
+# #         print(f"{a} is even")
+# #     else:   
+# #         print(f"{a} is odd") 
 
 
 
-# num=float(input("Enter a number: "))
-# if num==0:
-#     print("zero")   
-# elif num%2==0:
-#     print("even")
-# elif num%2!=0:
-#     print("odd")
+# # num=float(input("Enter a number: "))
+# # if num==0:
+# #     print("zero")   
+# # elif num%2==0:
+# #     print("even")
+# # elif num%2!=0:
+# #     print("odd")
 
-# def add():
-#     list=[1,2,3,4,5,6,7,8,9,10]
-#     num=int(input("Enter a number: "))
-#     list.append (num)
-#     print(list) 
+# # def add():
+# #     list=[1,2,3,4,5,6,7,8,9,10]
+# #     num=int(input("Enter a number: "))
+# #     list.append (num)
+# #     print(list) 
 
-# add()
-
-
-def add(a,b):
-    sum=a+b
-    print(sum)
+# # add()
 
 
-
-c=int(input("Enter 1st number: "))
-d=int(input("Enter 2nd number: "))
-
-add(c,d)
+# # def add(a,b):
+# #     sum=a+b
+# #     print(sum)
 
 
-c=10
-d=20
-add(c,d)
+
+# # c=int(input("Enter 1st number: "))
+# # d=int(input("Enter 2nd number: "))
+
+# # add(c,d)
+
+
+# # c=10
+# # d=20
+# # add(c,d)
+
+# # list=[12,44,53,2,5,6,23,56,8,9,94]
+# # list.sort(reverse=True)
+# # print(list)
+
+# # list=[12,44,53,2,5,6,23,56,8,9,94]
+# # list.reverse()
+# # print(list)
+
+# # list=[12,44,53,2,5,6,23,56,8,9,94]
+# # list.insert(3,100)a
+# # print(list)
+
+# list=[12,44,53,2,5,6,23,56,8,9,94]
+# list.pop(2)
+# print(list)
