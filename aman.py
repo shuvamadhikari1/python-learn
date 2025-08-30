f268bf68f383cb9fe486c16f477847b501910e58
HP <shuvamadhikari74@gmail.com>
2025-08-30 19:57:10 +0545

    naya add

--- aman.py
+++ aman.py
@@ -1,6 +1,41 @@
-i=[1,2,3,4,5,6,7,8,9,10]
-for a in i:
-    if a%2==0:  
-        print(f"{a} is even")
-    else:   
-        print(f"{a} is odd") 
+# i=[1,2,3,4,5,6,7,8,9,10]
+# for a in i:
+#     if a%2==0:  
+#         print(f"{a} is even")
+#     else:   
+#         print(f"{a} is odd") 
+
+
+
+# num=float(input("Enter a number: "))
+# if num==0:
+#     print("zero")   
+# elif num%2==0:
+#     print("even")
+# elif num%2!=0:
+#     print("odd")
+
+# def add():
+#     list=[1,2,3,4,5,6,7,8,9,10]
+#     num=int(input("Enter a number: "))
+#     list.append (num)
+#     print(list) 
+
+# add()
+
+
+def add(a,b):
+    sum=a+b
+    print(sum)
+
+
+
+c=int(input("Enter 1st number: "))
+d=int(input("Enter 2nd number: "))
+
+add(c,d)
+
+
+c=10
+d=20
+add(c,d)
