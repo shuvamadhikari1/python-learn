a4896ebdd7c2f2827701b35feb67f75494a33900
HP <shuvamadhikari74@gmail.com>
2025-09-02 02:50:24 +0545

    loops

---
+++ loops.py
@@ -0,0 +1,64 @@
+a= float(input("Enter the first number: "))
+b= float(input("Enter the second number: "))
+count = 1
+if a > b:
+    while a >= b:
+        print("hello world")
+        a -= 1
+
+# # count = 1
+# # while count <= 10:
+# #     print("hello world")
+# #     count += 1
+
+
+# #qn1
+# # i= 1
+# # while i <= 10:
+# #     print(i)
+# #     i += 1  
+
+#qn2
+# # i = 100
+# # while i >= 1:
+# #     print(i)
+# #     i -= 1
+
+# # i= 1
+# # while i<= 10:
+# #     print(i**2)
+# #     i += 1  
+
+#qn3
+# n= int(input("Enter a number: "))
+# i=1
+# while i <= 10:
+#     print(i*n)
+#     i += 1
+
+#qn4
+
+# num= []
+# i=1
+# while i <= 10:
+#     num.append(i**2)
+#     i+=1
+# print(num)
+
+# num= []
+# i=1
+# while i <= 10:
+#     num.append(i**2)
+#     i+=1
+
+#     mytuple= tuple(num)
+# print(mytuple)
+
+   
+# i = 1
+# while i <= 10:
+#     if i == 3:
+#         print("hello")
+#         break  # exit the loop when i is 3
+#     print(i)  # optional: prints numbers before 3
+#     i += 1
