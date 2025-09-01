ff744ce40dc67c9dd0926d7d621a85582f3faeb9
HP <shuvamadhikari74@gmail.com>
2025-09-02 00:32:35 +0545

    dict

---
+++ dict.py
@@ -0,0 +1,10 @@
+
+
+dict = {"TarakMetha":{"Views":"1m", "Comments":"10k"} , "CID": {"Views":"0.5m", "Comments":"15k"}}
+
+
+
+
+
+
+print(dict["TarakMetha"]["Comments"])
---
+++ questions.py
@@ -0,0 +1,45 @@
+# # # WAP to create a list of movies and add three movie names to it.
+# # movie=[]
+
+# # mov1=input("Enter the name of the movie: ")
+# # mov2=input("Enter the name of the movie: ")
+# # mov3=input("Enter the name of the movie: ")
+
+# # movie.append(mov1)
+# # movie.append(mov2)
+# # movie.append(mov3)
+# # print(movie)
+
+
+# # WAP to check if a list cotains palindrome or not.
+# # list=[1,2,3,2,1,3]
+# # list1=list.copy()
+# # list1.reverse()
+# # if list==list1:
+# #     print("The list is palindrome")
+# # else:
+# #     print("The list is not palindrome")
+
+# # list=["C","D","A","A","B","B","A"]
+
+# # list1=list.count("A")
+# # print(list1)
+
+
+# list2=["C","D","A","A","B","B","A"]
+# list2.sort()
+# set1=set(list2)
+# list1=list(set1)
+# list1.sort()
+# print(list1)
+
+
+# # list1=list.remove("B")
+# # list1=list.remove("A")
+# # list1=list.remove("A")
+# # print(list)
+# # print(list1)
+
+
+
+
