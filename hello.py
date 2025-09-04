def554040bffb2e1aba880bc405862ff99392552
HP <shuvamadhikari74@gmail.com>
2025-09-04 20:37:41 +0545

    aaaaaa

--- hello.py
+++ hello.py
@@ -1,9 +1,2 @@
-
-def factorial(n):
-    if n==0 or n==1:
-        return 1
-    else:
-        return n*factorial(n-1)
-    
-n=int(input("Enter a number: "))
-print("Factorial of",n,"is",factorial(n))
+import os
+api_key = os.getenv("XAI_API_KEY")
